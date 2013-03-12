94017b1f7a6093e973acfbc28f67e326fdacbbdc
Paul Belanger <paul.belanger@polybeacon.com>
2013-03-11 21:03:15 -0400

    Import project into kickstandproject
    This commit properly adds trunk gating tests for sarlacc
    
    Change-Id: I0d3071b1e6c5d770188ef5481199a2f84223c6e3
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ doc/source/conf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 PolyBeacon, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+
+import os
+import sys
+
+from sarlacc.version import SARLACC_VERSION as sarlacc_version
+
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Sarlacc'
+copyright = u'2013, Paul Belanger'
+release = sarlacc_version.version_string_with_vcs()
+version = sarlacc_version.canonical_version_string()
+exclude_patterns = []
+pygments_style = 'sphinx'
+html_theme = 'default'
+htmlhelp_basename = 'sarlaccedoc'
+
+latex_elements = {
+}
+
+latex_documents = [
+  ('index', 'Sarlacc.tex', u'Sarlacc Documentation',
+   u'Paul Belanger', 'manual'),
+]
+
+man_pages = [
+    ('index', 'Sarlacc', u'Sarlacc Documentation',
+     [u'Paul Belanger'], 1)
+]
+
+texinfo_documents = [
+  ('index', 'Sarlacc', u'Sarlacc Documentation',
+   u'Paul Belanger', 'Sarlacc', 'One line description of project.',
+   'Miscellaneous'),
+]
