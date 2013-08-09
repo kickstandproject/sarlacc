8ca0e0fbdae2cba4c765e7fe4cdb3bae5b691bbf
Paul Belanger <paul.belanger@polybeacon.com>
2013-08-09 15:53:26 -0400

    Properly display license header in source files
    Change-Id: I65017985b59e2414ec3d00562560dc0ac9632b52
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- sarlacc/test.py
+++ sarlacc/test.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 # Copyright (C) 2013 PolyBeacon, Inc.
+#
+# Author: Paul Belanger <paul.belanger@polybeacon.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,8 +12,9 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import testtools
 
