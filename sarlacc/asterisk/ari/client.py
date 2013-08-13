ee60e7a15b0ee68e3ee2818801adb857aaa42900
Paul Belanger <paul.belanger@polybeacon.com>
2013-08-13 16:58:40 -0400

    Simple documentation fix
    This is more to test our tarballs job on Jenkins
    
    Change-Id: I1b97b62001708b39c9e8e141ae5d686ab0fd0a99
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- sarlacc/asterisk/ari/client.py
+++ sarlacc/asterisk/ari/client.py
@@ -16,6 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Asterisk ARI client
+"""
+
 from sarlacc.asterisk.ari import asterisk
 from sarlacc.common import http
 
